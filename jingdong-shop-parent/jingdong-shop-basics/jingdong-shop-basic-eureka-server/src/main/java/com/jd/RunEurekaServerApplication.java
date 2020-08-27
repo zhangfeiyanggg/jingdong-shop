113bf6814b44054d96dca947d62ba12be46adb6d
zhangfeiyanggg <zhangfeiyanggggg@163.com>
2020-08-27 14:47:33 +0800

    aaaaa

--- jingdong-shop-parent/jingdong-shop-basics/jingdong-shop-basic-eureka-server/src/main/java/com/jd/RunEurekaServerApplication.java
+++ jingdong-shop-parent/jingdong-shop-basics/jingdong-shop-basic-eureka-server/src/main/java/com/jd/RunEurekaServerApplication.java
@@ -15,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class RunEurekaServerApplication {
     public static void main(String[] args) {
+        System.out.println("");
         SpringApplication.run(RunEurekaServerApplication.class);
     }
 }
