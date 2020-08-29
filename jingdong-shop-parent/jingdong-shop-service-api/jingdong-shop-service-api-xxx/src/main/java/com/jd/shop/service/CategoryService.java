cc804014774dfcf91942cfa2a8a6fb9c0a17947d
zhangfeiyanggg <zhangfeiyanggggg@163.com>
2020-08-29 09:09:45 +0800

    分类新增删除修改

--- jingdong-shop-parent/jingdong-shop-service-api/jingdong-shop-service-api-xxx/src/main/java/com/jd/shop/service/CategoryService.java
+++ jingdong-shop-parent/jingdong-shop-service-api/jingdong-shop-service-api-xxx/src/main/java/com/jd/shop/service/CategoryService.java
@@ -1,10 +1,11 @@
 package com.jd.shop.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jd.shop.base.Result;
 import com.jd.shop.entity.CategoryEntity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +22,16 @@ public interface CategoryService {
     @ApiOperation(value = "通过查询商品分类")
     @GetMapping(value = "category/list")
     Result<List<CategoryEntity>> getCategoryByPid(Integer pid);
+
+    @ApiOperation(value = "新增商品分类")
+    @PostMapping(value = "category/save")
+    Result<JSONObject> saveCategory(@RequestBody CategoryEntity categoryEntity);
+
+    @ApiOperation(value = "修改商品分类")
+    @PutMapping(value = "category/edit")
+    Result<JSONObject> updateCategory(@RequestBody CategoryEntity categoryEntity);
+
+    @ApiOperation(value = "删除商品分类")
+    @DeleteMapping(value = "category/delCategory")
+    Result<JSONObject> delCategory(Integer id);
 }
--- jingdong-shop-parent/jingdong-shop-service/jingdong-shop-service-xxx/src/main/java/com/jd/shop/service/impl/CategoryServiceImpl.java
+++ jingdong-shop-parent/jingdong-shop-service/jingdong-shop-service-xxx/src/main/java/com/jd/shop/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.jd.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jd.shop.mapper.CategoryMapper;
 import com.jd.shop.base.BaseApiService;
 import com.jd.shop.base.Result;
 import com.jd.shop.entity.CategoryEntity;
 import com.jd.shop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -35,4 +38,65 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
 
         return this.setResultSuccess(list);
     }
+
+    @Override
+    @Transactional
+    public Result<JSONObject> saveCategory(CategoryEntity categoryEntity) {
+
+        CategoryEntity parentEntity = new CategoryEntity();
+        //新增时获取到父类目id为0  即不是父级目录
+        parentEntity.setId(categoryEntity.getParentId());
+        //把新增的父类目id的状态0转换成1,让他成为父级目录
+        parentEntity.setIsParent(1);
+        //修改父类目状态
+        categoryMapper.updateByPrimaryKeySelective(parentEntity);
+        categoryMapper.insertSelective(categoryEntity);
+        return this.setResultSuccess();
+    }
+
+    @Override
+    @Transactional
+    public Result<JSONObject> updateCategory(CategoryEntity categoryEntity) {
+
+        categoryMapper.updateByPrimaryKeySelective(categoryEntity);
+
+        return this.setResultSuccess();
+    }
+
+    @Override
+    @Transactional
+    public Result<JSONObject> delCategory(Integer id) {
+
+
+        //通过当前id查询分类信息
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+        //判断是否有数据(安全)
+        if(categoryEntity.getId() == null){
+            return this.setResultError("当前id不存在");
+        }
+
+        //判断当前节点是否为父节点
+        if(categoryEntity.getIsParent() == 1){
+            return this.setResultError("当前节点为父节点");
+        }
+
+        //构建条件查询 通过当前被删除节点的parentid查询数据
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+        //如果查询出来的数据只有一条
+        if(list.size() == 1){
+            CategoryEntity parentEntity = new CategoryEntity();
+            parentEntity.setId(categoryEntity.getParentId());
+            //将父节点的isParent状态改为0
+            parentEntity.setIsParent(0);
+            categoryMapper.updateByPrimaryKeySelective(parentEntity);
+        }
+
+        categoryMapper.deleteByPrimaryKey(id);
+
+        return this.setResultSuccess();
+    }
+
+
 }
